86fa57b886b0cb80785b8cc0d57890e3a69ab792
Linellian <72004449+Linellian@users.noreply.github.com>
2020-10-01 12:31:46 +0300

    Update ft_percent_lower_uppercase.py

--- ft_percent_lower_uppercase.py
+++ ft_percent_lower_uppercase.py
@@ -2,8 +2,8 @@ def ft_percent_lower_uppercase(str):
     l = 0
     u = 0
     for i in str:
-        if i >= "a" and i <= "c":
+        if i >= "a" and i <= "c" or i >= "а" and i <= "я":
             l += 1
-        elif i >= "A" and i <= "C":
+        elif i >= "A" and i <= "C" or i >= "А" and i <= "Я":
             u += 1
     return int(l / u)
