64e533d244469048399c5084c12c347cefa286bd
Linellian <72004449+Linellian@users.noreply.github.com>
2020-11-12 12:17:35 +0300

    Update ft_join.py

--- ft_join.py
+++ ft_join.py
@@ -1,6 +1,13 @@
+def ft_len(lst):
+    c = 0
+    for i in lst:
+        c += 1
+    return c
+
+
 def ft_join(lst, sep = " "):
     r = ""
-    for i in range(len(lst) - 1):
+    for i in range(ft_len(lst) - 1):
         r += str(lst[i])
         r += sep
     r += str(lst[-1])
