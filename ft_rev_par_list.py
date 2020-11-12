760ee5468fd38b1859f1562057756cbe9b638574
Linellian <72004449+Linellian@users.noreply.github.com>
2020-11-12 11:22:12 +0300

    Delete ft_rev_par_list.py

--- ft_rev_par_list.py
+++
@@ -1,8 +0,0 @@
-def ft_rev_par_list(mass):
-    if len(mass) % 2 == 0:
-        for i in range(0, len(mass), 2):
-            mass[i], mass[i + 1] = mass[i + 1], mass[i]
-    else:
-        for i in range(0, len(mass) - 1, 2):
-            mass[i], mass[i + 1] = mass[i + 1], mass[i]
-    return mass
