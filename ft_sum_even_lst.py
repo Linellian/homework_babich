a84dad61edc074ecee19b4ff5ccce71ea117a5f8
Linellian <72004449+Linellian@users.noreply.github.com>
2020-11-12 12:28:07 +0300

    Update ft_sum_even_lst.py

--- ft_sum_even_lst.py
+++ ft_sum_even_lst.py
@@ -1,5 +1,13 @@
+def ft_len(str):
+    num = 0
+    for i in str:
+        num += 1
+    return num
+
+
 def ft_sum_even_lst(lst):
     r = 0
-    for i in range(0, len(lst), 2):
-        r += lst[i]
+    for i in range(ft_len(lst) - 1):
+        if i % 2 == 0:
+            r += lst[i]
     return r
